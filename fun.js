ce45e5d790cf3bbfbe2c0a6a5d853d2e83f856d0
alset4 <alekstefanov99@gmail.com>
2025-04-30 07:04:43 -0400

    init commit

---
+++ fun.js
@@ -0,0 +1,15 @@
+
+//making a test file to practice my programming in vim
+//
+//
+
+function main() 
+{
+	const res = 1 + 3;
+	let arr = Array(10);
+	arr[0] = res;
+	console.log(`array holds ${arr[0]} at index 0!`);
+	console.log("entering main function!");
+}
+
+main();
